c31d72a800d97e1b34c94af35cc43ba2d2c18ea1
Alyssa Doerscher <alyssadoerscher@Alyssas-MacBook-Pro.local>
2016-08-31 13:31:44 -0500

    started poem object

---
+++ script.js
@@ -0,0 +1,36 @@
+$document.ready(function(){
+
+  var poems = [
+    {
+    poet: "Michael Robins",
+    poem: "His Passion is Doves",
+    book: "Ladies & Gentlemen",
+    publisher: "Saturnalia Books, 2011"
+    lines: ["Bewildered, our pigeons flew ashore long ago", "The mull the rooms of old, coastal motels"]
+   },
+   {
+    poet: "C.D. Wright",
+    poem: "from Just Whistle, a valentine",
+    book: "Steal Away",
+    publisher: "Copper Canyon Press, 2002",
+    lines: ["The body, alive, not dead, but dormant, like a cave that had stopped growing, stirred up, awakened"]
+  },
+  {
+    poet: "Elizabeth Hughey",
+    poem: "What Bird",
+    book: "Sunday Houses The Sunday House",
+    publisher: "University of Iowa Press, 2007",
+    lines: ["Bulbs, gravel driveway.", "I had hyacinth on my mouth."]
+  },
+  {
+    poet: "Laura Miller",
+    poem: "Wake",
+    book: "Subject",
+    publisher: "University of California Press, 2005",
+    lines: ["wake: outside the frame beyond his 'fit'", "wake: frothed a blankness in the passage of what", "wake: we waited     (and scilence)"]
+  }
+  ]
+
+  
+
+})
